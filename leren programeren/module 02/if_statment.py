ee02b25a1c3f8f3d8cfa6e43cbae3ad8117afc05
Jelle fiere <99071834@mydavinci.nl>
2022-09-14 12:01:10 +0200

    elif-statement

--- leren programeren/module 02/if_statment.py
+++ leren programeren/module 02/if_statment.py
@@ -6,7 +6,6 @@ b = int(input('geef nog een ander heel nummer  '))
 if a > b:
     max = a
     print(f'a is het grootste getal:{max}')
-
-else :
-    max = b
-    print(f'b is het grootste getal:{max}')
+elif a < b :
+    min = a
+    print(f'a is het grootste getal:{min}')
