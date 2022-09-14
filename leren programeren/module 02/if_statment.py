845b249042c4e272d0c81a5817393b1005dad6a1
Jelle fiere <99071834@mydavinci.nl>
2022-09-14 12:16:10 +0200

    Min en Max

--- leren programeren/module 02/if_statment.py
+++ leren programeren/module 02/if_statment.py
@@ -5,10 +5,17 @@ b = int(input('geef nog een ander heel nummer  '))
 
 if a > b:
     max = a
+    min = b
     print(f'a is het grootste getal:{max}')
+    print(f'Het minimum is:{min}')
+    print(f'het maximum is {max}')
 elif a < b :
     min = a
-    print(f'a is het grootste getal:{min}')
+    max = b
+    print(f'a is het kleinste getal:{min}')
+    print(f'Het minimum is:{min}')
+    print(f'het maximum is {max}')
 else: 
     print(f'a en b zijn even groot')
-    
+    min = a
+    print(f'Het minimum en het maximum zijn:{min}')
