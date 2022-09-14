f7784319d8e5ce31d1bcf49db74f3c88e1da03b1
Jelle fiere <99071834@mydavinci.nl>
2022-09-14 11:22:13 +0200

    input en if-statement

---
+++ leren programeren/module 02/if_statment.py
@@ -0,0 +1,12 @@
+a = int(input('geef een heel nummer '))
+b = int(input('geef nog een ander heel nummer  '))
+
+
+
+if a > b:
+    max = a
+    print(f'a is het grootste getal:{max}')
+
+else :
+    max = b
+    print(f'b is het grootste getal:{max}')
---
+++ leren programeren/module 02/leeftijd.py
@@ -0,0 +1,10 @@
+leeftijd = int(input('hoe oud ben je?  ') )
+x3 = leeftijd * 3 
+print(f"als je 3 keer zo oud zou zijn, was je {x3} geweest!")
+
+
+uur = int(input('hoe laat is het in uren  '))
+minuten = int(input('hoe veel minuten is het?  '))
+einduur = 24 - uur
+eindminuten = 60 - minuten
+print (f'dan duurt het nog {einduur} uur en {eindminuten} minuten voor dat de dag voor bij is')
