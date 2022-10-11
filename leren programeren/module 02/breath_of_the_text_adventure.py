d841f44b62455d2c534ebbabd6f68756b20792d6
Jelle fiere <99071834@mydavinci.nl>
2022-10-11 11:59:07 +0200

    text adventure

--- leren programeren/module 02/breath_of_the_text_adventure.py
+++ leren programeren/module 02/breath_of_the_text_adventure.py
@@ -1,6 +1,4 @@
-from pickle import FALSE
 import random
-from this import d
 import time
 #shit is false lmao
 weapon_axe = False
@@ -9,12 +7,14 @@ weapon_black_sword = False
 weapon_2swords = False
 weapon_shield = False
 armor_royal_guards_armor = False
-
+item_food = False
+weapon_sword = False
+weapon_flamesword = False
 # function to print with delay
 def printDelay(t: str, d=2):
     time.sleep(d)
     print(t)
-#the game real
+#the game begining
 printDelay('You wake up in a ancient temple you don’t rember anything you walk out into a bright light')
 man_or_forest = input('You hear a voice of a woman telling you you have been sleep for the past 100 years you see a old man in the distence do you go towards him Y/N?  ').lower()
 if man_or_forest == 'yes' or man_or_forest == 'y':
@@ -24,9 +24,12 @@ if man_or_forest == 'yes' or man_or_forest == 'y':
     axe = input('You see a axe in the discence do you pick it up y/n  ').lower()
     if axe == 'yes' or axe == 'y' :
         weapon_axe = True
+    else:
+        printDelay('you did not pick up the axe')
     printDelay('You see a button and you press it a big tower emerges from under the building')
     printDelay('You climb down the tower and you see the old man again he tells you to go the shrine towards the east')
     printDelay('You go to the shrine and a old mashine comes to life when it notices you it has a red lazer pointed at you')
+    #puzzel and guardian
     run_or_fight_guardian = input('do you run away into the shrine or do you fight it?  ').lower()
     if run_or_fight_guardian == 'run away' or run_or_fight_guardian == 'run':
         printDelay('You run into the shrine and escape from the things lazer there is a puzzel you must do you look around and see 4 stones on the floor')
@@ -38,19 +41,24 @@ if man_or_forest == 'yes' or man_or_forest == 'y':
         puzel_elephant = input('In the raining swamp you put the...').lower()
         if puzel_bird == 'bird' and puzel_camel == 'camel' and puzel_elephant == 'elephant' and puzel_lizard == 'lizard':
             printDelay('the door infront of you opens')
-            printDelay('you find yourself in a treasure room witha orb in the middel ')
+            printDelay('you find yourself in a treasure room with a orb in the middel ')
             printDelay('you take the orb with you')
             printDelay('you walk out of the temple and see the old man again')
             printDelay('Good job young one now go to the temple of time i shall meet you there again and then he disapears')
             printDelay('you make your way to the temple of time to find the old man')
             printDelay('you find a big stone statue with a hole in could fit the glowing ball')
+            #chest part
             ball_in_statue = input('do you put it in Y/N?  ').lower()
             if ball_in_statue == 'y' or ball_in_statue == 'yes':
                 printDelay('you put the orb into the statue it opens a trap door behind you')
                 open_chest =input('there is a chest under the trap door do you open it? Y/N  ').lower()
                 if open_chest == 'yes' or 'y':
-                    print('you got some programming socks')
+                    printDelay('you got some programming socks')
                     armor_programming_socks = True
+                elif open_chest == 'no' or 'n':
+                    printDelay('you did not open the chest')
+            elif ball_in_statue == 'no' or 'n':
+                printDelay('you did not open the chest')
             printDelay('The old man tells you to come up the later to the roof')
             printDelay('"well young one i think its time for me to tell you who i really am"')
             printDelay('"i was king rhoam bosphoramus hyrule i was the last king of hyrule ')
@@ -58,6 +66,7 @@ if man_or_forest == 'yes' or man_or_forest == 'y':
             printDelay('you head towards the castle ')
             printDelay('you see two posible ways to enter')
             printDelay('you can enter trough the front gate or you could enter via the water')
+            #the castle
             how_enter_castle = input('how will you enter?  ').lower
             if how_enter_castle == 'water' or 'via the water' or 'via water' :
                 printDelay('You enter via the water you end up in the secret docking area of the castle you find a black sword')
@@ -67,11 +76,88 @@ if man_or_forest == 'yes' or man_or_forest == 'y':
                 armory_or_boss = input('do you fight them or do you keep walking?  ')
                 if armory_or_boss == 'fight' or 'fight them':
                     sneak_or_head_water = input('do you sneak attack or do you fight them head on?  ')
-                    if sneak_or_head_water =='sneak' or 'sneak attack':
+                    if sneak_or_head_water =='sneak' or sneak_or_head_water =='sneak attack':
                         printDelay('you sneak attack the guards and they die ')
                         printDelay('you loot the armory you got 2 swords a shield and some royal guards armor nice')
                         weapon_2swords = True
                         weapon_shield = True
                         armor_royal_guards_armor = True
+                    elif sneak_or_head_water == 'head on' or sneak_or_head_water =='attack head on' or sneak_or_head_water=='fight head on':
+                        printDelay('you decide to fight the guard head on')
+                        printDelay('sadly enough the guards are way stronger then you so you die')
+                        printDelay('ending 6 died to the guards in the armory')
+                        exit()
+                    else:
+                        printDelay('i gave you the options how did you mess that up')
+                        printDelay('ending 9 you put in a un usable comand')
+                        exit('dumb ass')
                     printDelay('then you walk to the boss room')
                     printDelay('you enter the boss room')
+            elif how_enter_castle == 'front gate' or how_enter_castle == 'gate':
+                printDelay('You quickly run towards the first entrance  into the castle you see you are in the dining room and find some food so you can heal')
+                item_food = True
+                printDelay('You also see a sword you take it with you')
+                weapon_sword = True
+                printDelay('you walk further and see the boss room')
+                printDelay('but to the felt of you you see a strong looking sword but there is a guard')
+                weapon_or_boss=input('do you go for the weapon or do you go to the boss room?  ')
+                if weapon_or_boss == 'boss room' or 'go to boss room':
+                    printDelay('you walk to the boss room')
+                elif weapon_or_boss == 'weapon' or weapon_or_boss =='go to weapon':
+                    sneak_or_head_gate =input('do you fight him head on or do you sneak attack him')
+                    if sneak_or_head_gate == 'fight him head on' or sneak_or_head_gate == 'fight head on' or sneak_or_head_gate == 'head on':
+                        printDelay('you decide to fight him head on')
+                        printDelay('but he is a lot stronger then you')
+                        printDelay('ending 5 you died to a guard')
+                        exit()
+                    elif sneak_or_head_gate == 'sneak' or sneak_or_head_gate == 'sneak attack' or sneak_or_head_gate == 'sneak attack him':
+                        printDelay('you sneak attack the guard and he dies')
+                        printDelay('you pick up the flaming sword')
+                        weapon_flamesword = True
+                        printDelay(' now you walk to the boss room')
+                    else:
+                        printDelay('i gave you the options how did you mess that up')
+                        printDelay('ending 9 you put in a un usable comand')
+                        exit('dumb ass')
+            else:
+                    printDelay('i gave you the options how did you mess that up')
+                    printDelay('ending 9 you put in a un usable comand')
+                    exit('dumb ass')
+        else:
+            printDelay('a trapdoor opens under you and you all to your death')
+            printDelay('ending 4 you failed the puzel')
+            exit()
+    elif run_or_fight_guardian == 'fight' or run_or_fight_guardian == 'fight it':
+        printDelay('you grab your weapon and run towards it to fight it sadly enough it shoots a lazer killing you before you can even reach it')
+        printDelay('ending 3 you died to the guardian lmao')
+        exit()
+    else:    
+        printDelay('i gave you the options how did you mess that up')
+        printDelay('ending 9 you put in a un usable comand')
+        exit('dumb ass')
+elif man_or_forest == 'n' or man_or_forest == 'no':
+    printDelay('you decide to not go to the old man and you go into the forest')
+    printDelay('after wandering in the forest for a while you find yourself near a kamp of bokoblins')
+    talus_or_boko =input('they spot you and ready there weapons do you fight or flee?  ').lower()
+    if talus_or_boko == 'fight' or 'fight them':
+        printDelay('you ready yourself for a fight sadly enough one of them has a bow and shoots you in the heart')
+        printDelay('you fall to the ground and die')
+        printDelay('ending 1 boko death')
+        exit()
+    elif talus_or_boko == 'flee' or talus_or_boko == 'run away':
+        printDelay('you run away deeper into the forest')
+        printDelay('you eventualy end up in a stony area and sit down on a stone')
+        printDelay('but the stone under you starts moving')
+        printDelay('it turns out you sat down on a stone stone talus and its not happy')
+        printDelay('it smashes you with its giant stone hands and you die')
+        printDelay('ending 2 stone talus')
+        exit()
+    else:
+        printDelay('i gave you the options how did you mess that up')
+        printDelay('ending 9 you put in a un usable comand')
+        exit('dumb ass')
+else:
+    printDelay('i gave you the options how did you mess that up')
+    printDelay('ending 9 you put in a un usable comand')
+    exit('dumb ass')
+printDelay('boss test 123 test')
