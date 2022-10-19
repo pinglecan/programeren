cb280b16cb1e6299a94f0db3d5041c192d50f443
Jelle fiere <99071834@mydavinci.nl>
2022-10-19 13:04:13 +0200

    robot arm 2

--- leren programeren/module 3/robotarm-python-2021-main/robotarm1.py
+++ leren programeren/module 3/robotarm-python-2021-main/robotarm1.py
@@ -3,6 +3,9 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 1')
 
 # Jouw python instructies zet je vanaf hier:
-
+robotArm.moveRight()
+robotArm.grab()
+robotArm.moveLeft()
+robotArm.drop()
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
