c26f5c9096b5f5a9d6c3d123081dd157f89f7ba0
Jelle fiere <99071834@mydavinci.nl>
2022-11-17 14:07:16 +0100

    raad eens

--- leren programeren/module 3/raadeens.py
+++ leren programeren/module 3/raadeens.py
@@ -2,13 +2,14 @@ from random import randint
 import os 
 from turtle import clear
 from time import sleep
-
+game = 0
 punt = 0
-for i in range (20):
+stop = False
+while game != 20 and stop == False:
     computer_numer = randint(1,1000)
     rounds = 0  
     while rounds <= 10:
-        player_number = int(input('type een nummer tussen 1 en 1000 of type quit om te stoppen  '))
+        player_number = int(input('type een nummer tussen 1 en 1000  '))
         if player_number < computer_numer:
             print('>')
         elif player_number > computer_numer:
@@ -17,7 +18,8 @@ for i in range (20):
             print('correct +1 point')
             sleep(3)
             punt += 1
-            break
+        if rounds == 10:
+            print('you lost')
         rounds += 1
         verschil = computer_numer - player_number
         if verschil < 0 :
@@ -26,13 +28,16 @@ for i in range (20):
             print('je bent erg warm')
         elif verschil <= 50:
             print('je bent warm')
-        if rounds == 10:
-            print('you lost')
     os.system('cls')
-    nog_een_keer = input('wil je nog een keer?  ').lower()
-    if nog_een_keer == 'nee':
+    game+=1
+    if game < 20:
+        nog_een_keer = input('wil je nog een keer?  ').lower()
+        if nog_een_keer == 'nee':
+                print(f'{punt} aantal punten')
+                stop = True
+        else :
             print(f'{punt} aantal punten')
-            exit()
-    print(f'{punt} aantal punten')
-    print('volgende ronde')         
+            print('volgende ronde')         
+    else:
+        print('bedankt')
     
