2afed0c612f91299702a32314abbeba984aa231f
Jelle fiere <99071834@mydavinci.nl>
2022-11-22 10:07:26 +0100

    (dagen)

---
+++ leren programeren/modulen 4/(dagen).py
@@ -0,0 +1,15 @@
+allendagen = ('maandag ', 'dinsdag ', 'woendsdag ', 'donderdag ', 'vrijdag ', 'zaterdag ', 'zondag ')
+for i in range (0,5):
+    print(allendagen[i])
+print('')
+for i in range(5, 7):
+    print(allendagen[i])
+print('')
+for i in range(0,7)[::-1] :
+    print(allendagen[i])
+print('')
+for i in range(0,5)[::-1]:
+    print(allendagen[i])
+print('')
+for i in range(5,7)[::-1]:
+    print(allendagen[i])
