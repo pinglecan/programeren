7592ec8cd500507a07ecd3c01c1d78946970d25c
Jelle fiere <99071834@mydavinci.nl>
2022-12-08 11:05:24 +0100

    wk af

--- leren programeren/challenges/wk_challenge.py
+++ leren programeren/challenges/wk_challenge.py
@@ -38,6 +38,7 @@ Wedstrijd {land_1} - {land_2} eindigde in {w1_doel_punten_land_1} - {w1_doel_pun
 {land_3}: punten:   {punten_land_3}  doelsaldo:  {doelsaldo_land3}
 ''')
 
+print(f'wat is de uitslag van wedstrijd 1 {land_2} vs {land_3}')
 w2_doel_punten_land_2 = int(input(f'hoeveel goals heeft {land_2} geschoord? '))
 w2_doel_punten_land_3 = int(input(f'hoeveel goals heeft {land_3} geschoord? '))
 
@@ -51,10 +52,32 @@ else:
     punten_land_3 += 3
 
 doelsaldo_land2 += w2_doel_punten_land_2 - w2_doel_punten_land_3
-doelsaldo_land3 += w2_doel_punten_land_3 - w2_doel_punten_land_3
+doelsaldo_land3 += w2_doel_punten_land_3 - w2_doel_punten_land_2
 
 print(f'''
 Wedstrijd {land_2} - {land_3} eindigde in {w2_doel_punten_land_2} - {w2_doel_punten_land_3} 
+{land_1}: punten:   {punten_land_1}  doelsaldo:  {doelsaldo_land1}
+{land_2}: punten:   {punten_land_2}  doelsaldo:  {doelsaldo_land2}
+{land_3}: punten:   {punten_land_3}  doelsaldo:  {doelsaldo_land3}
+''')
+print(f'wat is de uitslag van wedstrijd 1 {land_3} vs {land_1}')
+w3_doel_punten_land_3 = int(input(f'hoeveel goals heeft {land_3} geschoord? '))
+w3_doel_punten_land_1 = int(input(f'hoeveel goals heeft {land_1} geschoord? '))
+
+if w3_doel_punten_land_1 > w3_doel_punten_land_3:
+    winnaar_3 = land_1
+    punten_land_1 += 3
+    punten_land_3 += 0
+else:
+    winnaar_3 = land_3
+    punten_land_1+= 0
+    punten_land_3 += 3
+
+doelsaldo_land1 += w3_doel_punten_land_1 - w2_doel_punten_land_3
+doelsaldo_land3 += w3_doel_punten_land_3 - w3_doel_punten_land_1
+
+print(f'''
+Wedstrijd {land_3} - {land_1} eindigde in {w3_doel_punten_land_3} - {w3_doel_punten_land_1} 
 {land_1}: punten:   {punten_land_1}  doelsaldo:  {doelsaldo_land1}
 {land_2}: punten:   {punten_land_2}  doelsaldo:  {doelsaldo_land2}
 {land_3}: punten:   {punten_land_3}  doelsaldo:  {doelsaldo_land3}
@@ -63,13 +86,9 @@ Wedstrijd {land_2} - {land_3} eindigde in {w2_doel_punten_land_2} - {w2_doel_pun
 
 
 
-
-
-
-# print(f'''
-# wedstrijd | thuis | uit | doelpunten land 1 | doelpunten land 2| winnaar
-# 1         |{land_1} |{land_2} |        {w1_doel_punten_land_1}     |    {w1_doel_punten_land_2}    |          {winnaar_1} 
-# 2         |{land_2} |{land_3}|              |                  |
-# 3         |{land_1} |{land_3}|              |                  |
-# ''')
-# print('')
+print(f'''
+wedstrijd | thuis | uit | doelpunten land 1 | doelpunten land 2| winnaar
+1         |{land_1} |{land_2} |        {w1_doel_punten_land_1}     |    {w1_doel_punten_land_2}    |          {winnaar_1} 
+2         |{land_2} |{land_3}|  {w2_doel_punten_land_2}            |   {w2_doel_punten_land_3}     |          {winnaar_2}
+3         |{land_3} |{land_1}|   {w3_doel_punten_land_1}           |   {w3_doel_punten_land_3}     |          {winnaar_3}
+''')
