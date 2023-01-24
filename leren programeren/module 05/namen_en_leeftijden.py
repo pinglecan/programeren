ee5252979124d58e0f0e61ecd7a3fca818e6b544
Jelle Fiere <99071834@mydavinci.nl>
2023-01-24 12:47:00 +0100

    Update namen_en_leeftijden.py

--- leren programeren/module 05/namen_en_leeftijden.py
+++ leren programeren/module 05/namen_en_leeftijden.py
@@ -1,17 +1,23 @@
 def naam_leeftijd(x: list):
-    print('als u wilt stoppen type stop om the stoppen')
-    userinput_naam = input('wat is uw naam ').lower
-    userinput_leeftijd = input('wat is uw leeftijd ').lower
+    userinput_naam = input('wat is uw naam ').lower()
+    userinput_leeftijd = input('wat is uw leeftijd ').lower()
     namen_en_leeftijden.append({
     'naam' : userinput_naam,
     'leeftijd' : userinput_leeftijd
     })
-    if userinput_leeftijd or userinput_naam == 'stop':
-        return x
+    return x
 
 namen_en_leeftijden = []
 
-naam_leeftijd(namen_en_leeftijden)
 
+stop = False
 
+while not stop:
+    naam_leeftijd(namen_en_leeftijden)
+    stoppen =input('Toets enter om door te gaan of stop om te printen  ').lower()
+    if stoppen == 'stop':
+        stop = True
 print(namen_en_leeftijden)
+
+for naam in namen_en_leeftijden:
+    print(naam['naam'])
