9d078687ed77ec552b70fd8864455c7c0f9a6b5b
Jelle Fiere <99071834@mydavinci.nl>
2023-02-01 09:18:11 +0100

    Update computer guess.py

--- zelf dingen/computer guess.py
+++ zelf dingen/computer guess.py
@@ -4,7 +4,7 @@ ronde = 1
 
 while ronde > 0:
     ronde += 1
-    nummer = random.randint(1,100000)
+    nummer = random.randint(1,10000)
     print(nummer)
     if nummer == 20:
         print('Goed gedaan!')
