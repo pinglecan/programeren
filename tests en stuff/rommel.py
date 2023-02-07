78d48f525074b376fb37abfde67a0baa79bf4192
Jelle Fiere <99071834@mydavinci.nl>
2023-02-07 09:06:31 +0100

    Update rommel.py

--- tests en stuff/rommel.py
+++ tests en stuff/rommel.py
@@ -1,2 +1,28 @@
-vershil = 20 - 50 
-print(vershil)
+player_name = 'link'
+enemy_name = 'bokoblin'
+player_health = 500
+enemy_health = 300
+
+weapon_axe = 3
+weapon_stick = 1
+
+
+
+
+
+
+
+
+
+print(f'''
+|
+|   { player_name}  vs  {enemy_name}
+|
+|
+|   {player_health} hp   {enemy_health} hp
+|
+|   A( {weapon_axe} dmg
+|   B( {weapon_stick} dmg
+|
+|
+''')
