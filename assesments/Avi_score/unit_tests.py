499b7875928f335bd185581e6897df7ff510d505
Jelle Fiere <99071834@mydavinci.nl>
2023-04-20 11:15:56 +0200

    .

--- assesments/Avi_score/unit_tests.py
+++ assesments/Avi_score/unit_tests.py
@@ -4,17 +4,17 @@ from functies import *
 if getNumberOfCharacters('aap') == 3:
     print("Test geslaagd 1")
 else:
-    print("Deze test is niet geslaagd")
+    print("Deze test is niet geslaagd 1")
 
 if getNumberOfCharacters('aap.') == 3:
     print("Test geslaagd 2")
 else:
-    print("Deze test is niet geslaagd")
+    print("Deze test is niet geslaagd 2")
 
 if getNumberOfCharacters('i love big fat chicken') == 22:
     print("Test geslaagd 3")
 else:
-    print("Deze test is niet geslaagd")
+    print("Deze test is niet geslaagd 3")
 
 
 # test 2: getNumberOfSentences
@@ -23,7 +23,6 @@ if getNumberOfSentences(getText('easy')) == 14:
 else:
     print("Deze test is niet geslaagd")
 
-print(getNumberOfSentences(getText('data\john.txt')))
 if getNumberOfSentences(getText('data\john.txt')) == 3664: 
     print("Test geslaagd 5")
 else:
@@ -31,7 +30,6 @@ else:
 
 
 # test 3: getNumberOfWords
-print(getNumberOfWords(getText('data\difficult1.txt')))
 if getNumberOfWords(getText('data\difficult1.txt')) == 82:
     print("Test geslaagd 6")
 else:
@@ -42,4 +40,7 @@ if getNumberOfWords(getText('data\easy1.txt')) == 11:
 else:
     print("Deze test is niet geslaagd 7")
 
-# schrijf zelf nog een extra testen voor getNumberOfWords (gebruik test.txt).
+if getNumberOfWords(getText('data\john.txt')) == 18841:
+    print("Test geslaagd 8")
+else:
+    print("Deze test is niet geslaagd 8")
